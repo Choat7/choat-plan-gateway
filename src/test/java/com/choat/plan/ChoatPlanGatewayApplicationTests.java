fc12d7e0f98d74f005d998b1162d56f7a2ec24c8
choat7 <13760149002@163.com>
2023-05-27 22:52:38 +0800

    新增spring网关工程

---
+++ src/test/java/com/choat/plan/ChoatPlanGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choat.plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoatPlanGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
